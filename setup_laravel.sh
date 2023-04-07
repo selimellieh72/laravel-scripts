97157608e4db8fdd0b34e00334223f7bd286a083
Selim Ellieh <selim@Selims-MBP.lan>
2023-04-07 18:31:14 +0300

    Supress error warnings and fix syntax error

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -2,28 +2,28 @@
 echo "Wellcome to the Laravel setup script!"
 
 echo "Updating packages..."
-apt update > /dev/null
+apt update > /dev/null  2>/dev/null
 echo "Installing git, curl, php, mysql, composer, and laravel..."
-apt install -y software-properties-common ca-certificates lsb-release apt-transport-https > /dev/null
+apt install -y software-properties-common ca-certificates lsb-release apt-transport-https > /dev/null  2>/dev/null
 echo "Installation complete!"
 echo "Adding php repository..." 
-LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php 
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php > /dev/null  2>/dev/null
 echo "Repository added!"
 echo "Installing database dependencies..."
-apt install -y mysql-server php-mysql php-dom > /dev/null
+apt install -y mysql-server php-mysql php-dom > /dev/null 2>/dev/null
 echo "Installation complete!"
 cd /var/www/html
 read -p "Enter your github project url: " url
 git clone $url 
-echo "Project cloned!
+echo "Project cloned!"
 cd $(cat $url | sed -E 's/.*\/(.*)/\1/')
 echo "Installing composer..."
 wget -q -O composer-setup.php https://getcomposer.org/installer
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer > /dev/null
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer > /dev/null 2>/dev/null
 echo "Composer installed!"
 echo "Installing project dependencies..."
-composer install --ignore-platform-reqs > /dev/null
-composer update --ignore-platform-reqs > /dev/null
+composer install --ignore-platform-reqs > /dev/null 2>/dev/null
+composer update --ignore-platform-reqs > /dev/null  2>/dev/null
 echo "Dependencies installed!"
 cp .env.example .env
 echo "Please edit .env file"
