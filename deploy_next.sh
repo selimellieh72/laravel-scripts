bf2521f18b51bb0563c04fece6da520fe35b284b
Selim Ellieh <selim@Selims-MBP.lan>
2023-04-08 14:31:32 +0300

    Fixes

--- deploy_next.sh
+++ deploy_next.sh
@@ -29,28 +29,28 @@ echo "server {
         # Serve any static assets with NGINX
         location /_next/static {
             alias $project/.next/static;
-            add_header Cache-Control "public, max-age=3600, immutable";
+            add_header Cache-Control \"public, max-age=3600, immutable\";
         }
 
         location / {
             try_files \$uri.html \$uri/index.html # only serve html files from this dir
             @public
             @nextjs;
-            add_header Cache-Control "public, max-age=3600";
+            add_header Cache-Control \"public, max-age=3600\";
         }
 
         location @public {
-            add_header Cache-Control "public, max-age=3600";
+            add_header Cache-Control \"public, max-age=3600\";
         }
 
         location @nextjs {
             # reverse proxy for next server
             proxy_pass http://localhost:8080; 
             proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Upgrade \$http_upgrade;
             proxy_set_header Connection 'upgrade';
-            proxy_set_header Host $host;
-            proxy_cache_bypass $http_upgrade;
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
         }
 
         listen 80 default_server;
