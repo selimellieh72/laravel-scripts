b71306a95090818ed1dbce26f9682b6e24744563
Selim Ellieh <selim@Selims-MBP.lan>
2023-04-08 12:46:20 +0300

    Added build next script

---
+++ build_next.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Welome to the next build script!"
+echo "Installing nodejs, npm..."
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - > /dev/null 2>/dev/null
+apt install -y nodejs > /dev/null 2>/dev/null
+echo "Nodejs and npm installed!"
+echo "Node version: $(node -v)"
+read -p "Enter git repository URL: " repo
+echo "Cloning repository..."
+git clone $repo > /dev/null 2>/dev/null
+echo "Repository cloned!"
+cd $(cat $repo | sed -E 's/.*\/(.*)/\1/')
+echo "Installing dependencies..."
+npm install --force > /dev/null 2>/dev/null
+echo "Dependencies installed!"
